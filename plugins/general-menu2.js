69ad23fb249205580747c2301e2ce47a0a97b376
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-16 07:48:34 +0800

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -4,7 +4,7 @@ let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `ʜɪ, ᴀᴋᴜʟᴀʜ sʜᴇʟʙʏ🥶. ᴏʀᴀɴɢ ᴘᴀʟɪɴɢ ᴋᴜʟʟ ᴅᴀɴ ᴘᴀʟɪɴɢ ᴅɪɴɢɪɴ ᴅɪ ᴅᴜɴɪᴀ🥶. ʙᴏᴛ ʙᴇʟɪᴀᴜ ɪɴɪ ᴅɪʙᴜᴀᴛ ᴏʟᴇʜ ɴɢᴀғ ʀᴇɴғᴜɴɪx🥶,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ🥶\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://chat.whatsapp.com/JMv6TBnYGCe7NngMhLdGmb', 'ɢᴄ ʙᴏᴛ', null, null, [
 [`ᴍᴇɴᴜ`, '.m'],
-[`\nᴀᴛᴍɪɴ ɴɢᴇɴᴛᴏᴛ🥶`, 'Bilek']
+[`\nHai, Shelby🥶`, 'Bilek']
 ], m, {asLocation: true})
 }
 handler.help = ['menu']
