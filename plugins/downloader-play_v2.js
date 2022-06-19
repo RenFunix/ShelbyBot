4fb5439ae7308fcb20baff386f23cc12203e789d
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-19 12:35:25 +0800

    Update downloader-play_v2.js

--- plugins/downloader-play_v2.js
+++ plugins/downloader-play_v2.js
@@ -7,7 +7,7 @@ if (command == 'play.1') {
 conn.reply(m.chat, `*_⏳Wait,Audio Sedang Di Proses..._⏳*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: 'AUDIO',
-body: 'sʜᴇʟʙʏ ʙᴏᴛ',         
+body: 'Kurumi-MD',         
 previewType: 0, thumbnail: fs.readFileSync("./thumbnail.jpg"),
 sourceUrl: `https://chat.whatsapp.com/JMv6TBnYGCe7NngMhLdGmb`}}})
 let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text)
@@ -17,7 +17,7 @@ if (command == 'play.2') {
 conn.reply(m.chat, `*_⏳ Wait,Video Sedang Di Proses...⏳_*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: 'VIDEO',
-body: 'sʜᴇʟʙʏ ʙᴏᴛ',         
+body: 'Kurumi-MD',         
 previewType: 0, thumbnail: fs.readFileSync("./thumbnail.jpg"),
 sourceUrl: `https://chat.whatsapp.com/JMv6TBnYGCe7NngMhLdGmb`}}})
 let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp4?text="+text)
