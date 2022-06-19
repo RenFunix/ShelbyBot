751bc6d7f90da0dfd1075854f0d2811ce066f5b9
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-19 12:36:14 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -9,7 +9,7 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
 *Nickname:* ${nickname}
 *Description:* ${description}
 
-_© sʜᴇʟʙʏ ʙᴏᴛ_
+_© Kurumi-MD_
 `.trim(), m)
 }
 handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
