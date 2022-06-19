60cbcad93d8c433206e78ed9f6fae85058db9a8b
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-19 13:44:21 +0800

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -2,9 +2,9 @@ import moment from 'moment-timezone'
 let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `ᴋᴏɴɪᴄʜɪᴡᴀ, ᴀɴᴀᴛᴀ ᴋᴜʀᴜᴍɪ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ ᴏʟᴇʜ ʀᴇɴғᴜɴɪx-sᴀᴍᴀ. sᴇʙᴇʟᴜᴍ ᴍᴇᴍᴀᴋᴀɪ ʙᴏᴛ, ᴅɪᴍᴏʜᴏɴ ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴀᴄᴀ ʀᴜʟᴇs ᴅᴜʟᴜ ʏᴀ ><.\nᴊɪᴋᴀ sᴜᴅᴀʜ, ꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://chat.whatsapp.com/JMv6TBnYGCe7NngMhLdGmb', 'ɢᴄ ʙᴏᴛ', null, null, [
+conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `ᴋᴏɴɪᴄʜɪᴡᴀ, ᴀɴᴀᴛᴀ ʜᴜ-ᴛᴀᴏ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ ᴏʟᴇʜ ʀᴇɴғᴜɴɪx-sᴀᴍᴀ. sᴇʙᴇʟᴜᴍ ᴍᴇᴍᴀᴋᴀɪ ʙᴏᴛ, ᴅɪᴍᴏʜᴏɴ ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴀᴄᴀ ʀᴜʟᴇs ᴅᴜʟᴜ ʏᴀ ><.\nᴊɪᴋᴀ sᴜᴅᴀʜ, ꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://chat.whatsapp.com/JMv6TBnYGCe7NngMhLdGmb', 'ɢᴄ ʙᴏᴛ', null, null, [
 [`ᴍᴇɴᴜ`, '.m'],
-[`ᴋᴜʀᴜᴍɪ ᴡᴀɴɢʏ ><`, 'Bilek']
+[`ʜᴜ-ᴛᴀᴏ ᴡᴀɴɢʏ ><`, 'Bilek']
 ], m, {asLocation: true})
 }
 handler.help = ['menu']
